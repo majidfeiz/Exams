ac3269c2764927cf809d579581ce87d6e3fd5dc8
sermajid <sermajid3@yahoo.com>
2022-08-23 12:09:11 +0430

    Add web routes

---
+++ src/routes/web.php
@@ -0,0 +1,14 @@
+<?php
+use Illuminate\Support\Facades\Route;
+use majidfeiz\Exams\Http\controllers\ExamsController;
+
+Route::get('test', function(){
+    return 'Hello from the contact form package';
+});
+
+Route::prefix('exams')->group(function (){
+    Route::get('/',[ExamsController::class,'index'])->name('exams.index');
+    Route::get('/{exam}',[ExamsController::class,'show'])->name('exams.show');
+    Route::post('/',[ExamsController::class,'store'])->name('exams.store');
+
+});

c0311393138ba04c6df7ed97a6b3c3f025aa91cd
sermajid <sermajid3@yahoo.com>
2022-08-23 12:08:43 +0430

    Add ExamController

---
+++ src/Http/controllers/ExamsController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace majidfeiz\Exams\Http\controllers;
+
+use Illuminate\Http\Client\Request;
+use Illuminate\Routing\Controller;
+
+class ExamsController extends controller
+{
+
+    public function index()
+    {
+        //
+    }
+
+    public function show()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        // Let's assume we need to be authenticated
+        // to create a new post
+        if (! auth()->check()) {
+            abort (403, 'Only authenticated users can create new exams.');
+        }
+
+    }
+}

ef9ee09f258e7d24e1c889d8591e56d722726474
sermajid <sermajid3@yahoo.com>
2022-08-23 12:08:17 +0430

    Add Exam migration

---
+++ src/Database/migrations/2022_08_23_100000_create_exams_table.php
@@ -0,0 +1,37 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExamsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('exams', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->double('total_score',2);
+            $table->bigInteger('start_time');
+            $table->bigInteger('end_time');
+            $table->integer('random_questions')->default(0);
+            $table->integer('status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('exams');
+    }
+}
